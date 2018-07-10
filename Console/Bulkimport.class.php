16d5c6de327d23fa4c79252626592d9ea70e2027
Mohit Mishra <mmishra@sangoma.com>
2018-07-10 06:41:08 +0000

    Code indentation changed

--- Console/Bulkimport.class.php
+++ Console/Bulkimport.class.php
@@ -6,51 +6,52 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 class Bulkimport extends Command {
-    protected function configure(){
-        $this->setName('bulkimport')
-        ->setAliases(array('bi'))
-        ->setDescription('This command is used to import extensions and dids')
-        ->setDefinition(array(
-            new InputOption('type', 't', InputOption::VALUE_REQUIRED, 'Type of file'),
-            new InputArgument('filename', InputArgument::REQUIRED, 'Filename', null),
-	     new InputOption('replace', null, InputOption::VALUE_NONE, 'To replace existing values'),))
-        ->setHelp('Import a file: fwconsole bulkimport --type=[extensions|dids] filename.csv --replace(Replace the existing values)');
-    }
-    protected function execute(InputInterface $input, OutputInterface $output){
-        $filename = $input->getArgument('filename');
-        $type = $input->getOption('type');
-        if($input->getOption('replace')){
-	   $replace = true;
-	 }
-	 if(file_exists($filename)){
-          $data = \FreePBX::Bulkhandler()->fileToArray($filename);
-        }else{
-          $output->writeln('<error>The specified file does not exist or we cannot read it</error>');
-          return false;
-        }
-        if(!$data){
-          $output->writeln('<error>The file provided did not process properly. Check the file formatting</error>');
-          return false;
-        }
-        switch ($type) {
-          case 'dids':
-            $output->writeln('Importing bulk dids');
-            $ret = \FreePBX::Bulkhandler()->import('dids', $data, $replace);
-          break;
-          case 'extensions':
-            $output->writeln('Importing bulk extensions');
-            $ret = \FreePBX::Bulkhandler()->import('extensions', $data, $replace);
-          break;
-          default:
-            $output->writeln('<error>You must specify the file type of --type=dids or --type=extensions</error>');
-            return false;
-          break;
-        }
-        if(!$ret){
-          $output->writeln('<error>The import failed</error>');
-          return false;
-        }else{
-          return true;
-        }
-    }
+	protected function configure(){
+		$this->setName('bulkimport')
+		->setAliases(array('bi'))
+		->setDescription('This command is used to import extensions and dids')
+		->setDefinition(array(
+			new InputOption('type', 't', InputOption::VALUE_REQUIRED, 'Type of file'),
+			new InputArgument('filename', InputArgument::REQUIRED, 'Filename', null),
+			new InputOption('replace', null, InputOption::VALUE_NONE, 'To replace existing values'),))
+		->setHelp('Import a file: fwconsole bulkimport --type=[extensions|dids] filename.csv --replace(Replace the existing values)');
+	}
+	protected function execute(InputInterface $input, OutputInterface $output){
+		$filename = $input->getArgument('filename');
+		$type = $input->getOption('type');
+		if ($input->getOption('replace')) {
+			$replace = true;
+		}
+		if (file_exists($filename)) {
+			$data = \FreePBX::Bulkhandler()->fileToArray($filename);
+		} else {
+			$output->writeln('<error>The specified file does not exist or we cannot read it</error>');
+			return false;
+		}
+		if (!$data) {
+			$output->writeln('<error>The file provided did not process properly. Check the file formatting</error>');
+			return false;
+		}
+
+		switch ($type) {
+			case 'dids':
+				$output->writeln('Importing bulk dids');
+				$ret = \FreePBX::Bulkhandler()->import('dids', $data, $replace);
+			break;
+			case 'extensions':
+				$output->writeln('Importing bulk extensions');
+				$ret = \FreePBX::Bulkhandler()->import('extensions', $data, $replace);
+			break;
+			default:
+				$output->writeln('<error>You must specify the file type of --type=dids or --type=extensions</error>');
+			return false;
+			break;
+		}
+		if (!$ret) {
+			$output->writeln('<error>The import failed</error>');
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
