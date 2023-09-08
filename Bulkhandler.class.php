a874702a9def80b7aa7443a1b4f4561a5033044e
Prashanth Mohan <prashanthm@sangoma.com>
2023-09-08 17:09:39 +0530

    Resolved compatibility issues pertaining to the PHP 8.2

--- Bulkhandler.class.php
+++ Bulkhandler.class.php
@@ -1,6 +1,7 @@
 <?php
 // vim: set ai ts=4 sw=4 ft=php:
 namespace FreePBX\modules;
+#[\AllowDynamicProperties]
 class Bulkhandler implements \BMO {
 	public function __construct($freepbx = null) {
 		if ($freepbx == null) {
